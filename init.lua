7657a1a956e315dad9673197c69c72ddb2e168ac
Filippo Ragazzo <filipporagazzo89@gmail.com>
2024-11-08 13:02:02 +0100

    Add GitHub Copilot

--- init.lua
+++ init.lua
@@ -145,6 +145,7 @@ require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
   'rafi/awesome-vim-colorschemes', -- Collection of awesome color schemes for Vim, merged for quick use
+  'github/copilot.vim', -- GitHub Copilot
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
