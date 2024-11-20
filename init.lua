0f52319cda3cf5b9a15d136a324f295e907398e7
Filippo Ragazzo <filipporagazzo89@gmail.com>
2024-11-20 13:31:13 +0100

    Add Harpoonline

--- init.lua
+++ init.lua
@@ -192,6 +192,22 @@ require('lazy').setup({
   },
 
   {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'abeldekat/harpoonline', version = '*' },
+    config = function()
+      local Harpoonline = require 'harpoonline'
+      Harpoonline.setup {
+        on_update = function()
+          require('lualine').refresh()
+        end,
+      }
+
+      local lualine_c = { Harpoonline.format, 'filename' }
+      require('lualine').setup { sections = { lualine_c = lualine_c } }
+    end,
+  },
+
+  {
     'f-person/git-blame.nvim',
     -- load the plugin at startup
     event = 'VeryLazy',
